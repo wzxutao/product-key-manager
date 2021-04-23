bb4f66812cd2b0eeca49a45de411aaa886fb857f
wzxutao <wzxutao@gmail.com>
2021-04-23 11:46:37 +0800

    init

---
+++ src/main/java/cn/rypacker/productkeymanager/ProductKeyManagerApplication.java
@@ -0,0 +1,13 @@
+package cn.rypacker.productkeymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductKeyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductKeyManagerApplication.class, args);
+	}
+
+}
