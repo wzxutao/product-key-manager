834a6b4794601151e277935f8372d0b0bece5ce7
wzxutao <wzxutao@gmail.com>
2021-04-26 11:46:41 +0800

    ! adds equals/hashcode for JsonRecord

--- src/main/java/cn/rypacker/productkeymanager/models/JsonRecord.java
+++ src/main/java/cn/rypacker/productkeymanager/models/JsonRecord.java
@@ -62,6 +62,20 @@ public class JsonRecord {
     }
 
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        JsonRecord that = (JsonRecord) o;
+
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 
     @Override
     public String toString() {
