7fc553e44a7e534c128df1611b2b9ff93784ec41
wzxutao <wzxutao@gmail.com>
2021-05-08 17:13:12 +0800

    change updater: remove ./src then copy

--- a_update.bat
+++ a_update.bat
@@ -3,6 +3,11 @@ for /f "tokens=1-3*" %%a in ("%*") do (
     set dst=%%b
 )
 
+mkdir %dst%"/newEmptyFolder"
+robocopy %dst%"/newEmptyFolder" %dst%"/src" /MIR
+rmdir %dst%"/newEmptyFolder"
+rmdir %dst%"/src"
+
 robocopy %src% %dst% /E
 echo Update Successful
 run.bat
