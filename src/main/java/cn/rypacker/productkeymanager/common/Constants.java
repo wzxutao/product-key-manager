51b602267629c83a9fbbfb9d3f62845073c04e41
Nikolai Krukov <43370430+wzxutao@users.noreply.github.com>
2024-01-18 21:29:37 +0800

    working on jwt

---
+++ src/main/java/cn/rypacker/productkeymanager/common/Constants.java
@@ -0,0 +1,8 @@
+package cn.rypacker.productkeymanager.common;
+
+public class Constants {
+
+    public static final String ADMIN_AUTH_COOKIE_KEY = "auth";
+
+    public static final String NORMAL_AUTH_COOKIE_KEY = "normalAuth";
+}
