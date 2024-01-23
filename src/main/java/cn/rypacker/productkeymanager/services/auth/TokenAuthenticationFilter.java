4a212967971a96de8f90782ccb169cf6806114fc
taxu <taxu@ebay.com>
2024-01-23 14:45:49 +0800

    fix token filter

--- src/main/java/cn/rypacker/productkeymanager/services/auth/TokenAuthenticationFilter.java
+++ src/main/java/cn/rypacker/productkeymanager/services/auth/TokenAuthenticationFilter.java
@@ -1,49 +1,45 @@
 package cn.rypacker.productkeymanager.services.auth;
 
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
-import org.springframework.security.web.authentication.ForwardAuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.rememberme.InvalidCookieException;
+import org.springframework.security.core.context.SecurityContextHolder;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Arrays;
 
 import static cn.rypacker.productkeymanager.common.Constants.COOKIE_KEY_ADMIN_AUTH;
 
 
-public class TokenAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+public class TokenAuthenticationFilter implements Filter {
+
+    private final AuthenticationManager authenticationManager;
 
     public TokenAuthenticationFilter(AuthenticationManager authenticationManager) {
-        super("/**", authenticationManager);
-        setAuthenticationSuccessHandler(new NullAuthenticationSuccessHandler());
+        this.authenticationManager = authenticationManager;
     }
 
 
     @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
-        var cookies = request.getCookies();
-        if (cookies == null) {
-            throw new InvalidCookieException("no cookies");
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if(!HttpServletRequest.class.isAssignableFrom(request.getClass())) {
+            chain.doFilter(request, response);
+            return;
         }
 
-        var token = Arrays.stream(cookies).filter(cookie -> cookie.getName().equals(COOKIE_KEY_ADMIN_AUTH)).findFirst().orElseThrow(() ->
-                new InvalidCookieException("no token")).getValue();
+        var cookies = ((HttpServletRequest) request).getCookies();
+        if(cookies == null) {
+            chain.doFilter(request, response);
+            return;
+        }
 
-//        var username = Arrays.stream(cookies).filter(cookie -> cookie.getName().equals(COOKIE_KEY_USERNAME)).findFirst().orElseThrow(() ->
-//                new InvalidCookieException("no username")).getValue();
+        var optionalToken = Arrays.stream(cookies).filter(cookie -> cookie.getName().equals(COOKIE_KEY_ADMIN_AUTH)).findFirst();
 
-        return getAuthenticationManager().authenticate(new CustomAuthenticationToken(token));
-    }
-
-    @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+        optionalToken.map(cookie -> {
+            var token = authenticationManager.authenticate(new CustomAuthenticationToken(cookie.getValue()));
+            SecurityContextHolder.getContext().setAuthentication(token);
+            return null;
+        });
         chain.doFilter(request, response);
     }
 }
