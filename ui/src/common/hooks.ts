9d504f5edac5e335a486580a7e5246135bfbf9ce
Nikolai Krukov <43370430+wzxutao@users.noreply.github.com>
2024-01-31 21:04:29 +0800

    adding date input

--- ui/src/common/hooks.ts
+++ ui/src/common/hooks.ts
@@ -1,5 +1,6 @@
 import { useCookies } from "react-cookie";
 import { COOKIE_KEY_ADMIN_AUTH, COOKIE_KEY_USERNAME } from "./constants";
+import React from "react";
 
 export const useUsername = (): string | null => {
     const [cookies, ,] = useCookies([COOKIE_KEY_USERNAME])
@@ -10,4 +11,14 @@ export const useUsername = (): string | null => {
 export const useIsAdmin = (): boolean => {
     const [cookies, ,] = useCookies([COOKIE_KEY_ADMIN_AUTH])
     return cookies[COOKIE_KEY_ADMIN_AUTH]
+}
+
+export function useCallbackRef<T>(): [T | null, (node: T) => void] {
+    const [ref, setRef] = React.useState<T | null>(null);
+
+    const attachRef = React.useCallback((node: T) => {
+        setRef(node);
+    }, []);
+
+    return [ref, attachRef]
 }
--- ui/src/routes/keygen/KeyGenPage.tsx
+++ ui/src/routes/keygen/KeyGenPage.tsx
@@ -2,14 +2,65 @@ import React from 'react';
 
 import './KeyGenPage.less'
 import SnackbarAlert, { useAlert } from '../../components/SnackbarAlert';
-import { Stack } from '@mui/material';
+import { Checkbox, FormControl, FormControlLabel, FormHelperText, Grid, Input, InputLabel, Stack, TextField } from '@mui/material';
+import { Label } from '@mui/icons-material';
+import { useCallbackRef } from '../../common/hooks';
+
+const INPUT_DATE_KEY = "__date";
 
 export default function KeyGenPage() {
     const [alertMsg, handleAlert] = useAlert();
 
+    const [todayChecked, setTodayChecked] = React.useState<boolean>(true);
+    const [dateValid, setDateValid] = React.useState<boolean>(true);
+
+    const [formRef, attachFormRef] = useCallbackRef<HTMLFormElement>();
+
+    const handleTodayCheckboxChange = React.useCallback((ev: React.ChangeEvent<HTMLInputElement>) => {
+        if (formRef === null) return;
+
+        const checked = ev.target.checked;
+        setTodayChecked(checked);
+
+        const today = new Date();
+        (formRef.elements.namedItem(INPUT_DATE_KEY)! as HTMLInputElement).value =
+            checked
+                ? '' + today.getFullYear().toString().slice(-2) +
+                ('0' + (today.getMonth() + 1)).slice(-2) +
+                ('0' + today.getDate()).slice(-2)
+                : '';
+    }, [formRef]);
+
+    const validateDate = React.useCallback((ev: React.FocusEvent<HTMLInputElement>) => {
+        console.log("blur")
+        const regex = /^\d{2}(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])$/;
+        setDateValid(regex.test(ev.target.value));
+    }, [])
+
     return (<>
         <SnackbarAlert msg={alertMsg} />
-        <Stack id="KeyGenPage">
+        <Stack id="KeyGenPage" >
+            <Grid
+                id="key-gen-form"
+                component="form"
+                container
+                ref={attachFormRef}
+            >
+                <div className="key-gen-form-row">
+                    <Grid item component={TextField} className='mandatory-key' variant="filled" disabled xs={1} value='日期' />
+                    <FormControlLabel control={
+                        <Checkbox value={todayChecked}
+                            onChange={handleTodayCheckboxChange} />}
+                        sx={{ padding: '0 20px' }}
+                        label="今天" />
+                    <Grid item component={Input}
+                        placeholder='YYMMDD'
+                        id={INPUT_DATE_KEY}
+                        onBlur={validateDate}
+                        error={!dateValid}
+                        disabled={todayChecked} xs={8} />
+                </div>
+            </Grid>
         </Stack>
     </>)
 }
