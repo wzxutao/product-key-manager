e57280a1b5b2d7f4eec03035dd03c7bd73f6a9c6
Nikolai Krukov <43370430+wzxutao@users.noreply.github.com>
2024-02-25 21:52:21 +0800

    update port

--- ui/src/common/constants.ts
+++ ui/src/common/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = `http://${window.location.hostname}:8081`;
+export const API_URL = `http://${window.location.hostname}:8084`;
 
 export const COOKIE_KEY_NORMAL_AUTH = "normalAuth";
 export const COOKIE_KEY_ADMIN_AUTH = "auth";
